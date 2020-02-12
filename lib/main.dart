49ffdbdbc3569f023b14dec98d4336af54962178
Takaaki Shirai <lain.op.rai@gmail.com>
2020-02-13 01:28:35 +0900

    Theme color: ORANGE

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       ],
       title: 'Lifely',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: MyHomePage(title: 'Lifely'),
     );
